71b977365daf8c8e3b8a830ad06fa39b01c31aaf
김주희 <woolfie@naver.com>
2024-05-02 22:35:32 +0900

    Test 초기화 / 후처리 작업

--- src/main/java/com/example/onion/CalculatorTest.java
+++ src/main/java/com/example/onion/CalculatorTest.java
@@ -1,19 +1,35 @@
 package com.example.onion;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+    private Calculator cal;
+
+    @Before //초기화 작업
+    public void setup(){
+        cal = new Calculator();
+        System.out.println("before");
+    }
+
     @Test
     public void add(){
-        Calculator cal = new Calculator();
         assertEquals(9, cal.add(6,3));
+        System.out.println("add");
     }
 
     @Test
     public void subtract(){
-        Calculator cal = new Calculator();
         assertEquals(3, cal.subtract(6,3));
+        System.out.println("subtract");
+    }
+
+    @After //후처리 작업
+    public void teardown(){
+        System.out.println("teardown");
     }
 }
--- src/main/java/com/example/onion/OnionApplication.java
+++ src/main/java/com/example/onion/OnionApplication.java
@@ -1,6 +1,5 @@
 package com.example.onion;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
