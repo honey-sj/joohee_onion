b10539e1fa257736fbbdba643c9d9f34a434e8b6
김주희 <woolfie@naver.com>
2024-05-05 21:31:16 +0900

    리팩토링

--- src/main/java/com/example/calculator/StringCalculator.java
+++ src/main/java/com/example/calculator/StringCalculator.java
@@ -8,34 +8,57 @@ public class StringCalculator {
 
         int add = 0;
 
-        //null 체크를 먼저 해줘야 빈값체크를 할 수가 있다. 순서 주의!
-        if(str == null){
-            return 0;
-        }
-
-        if(str.isEmpty()){
+        if(isBlank(str)){
             return 0;
         }
 
         String[] strArr = null;
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+        Matcher m = getMatcher(str);
 
         if (m.find()){
             String s1 = m.group(1);
             strArr = m.group(2).split(s1);
-        }
-        if(!m.find()){
-            strArr = str.split(",|:|\n");
+        } else {
+            strArr = getSplit(str);
         }
 
-        for (String s : strArr) {
-            int number = Integer.parseInt(s);
-            if(number < 0){
-                throw new RuntimeException();
-            }
-            add += number;
+        return sum(toInts(strArr));
+    }
+
+    private static Matcher getMatcher(String str) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str);
+        return m;
+    }
+
+    private static int[] toInts(String[] values){
+        int[] numbers = new int[values.length];
+        for(int i = 0 ; i < values.length ; i++){
+            numbers[i]  = checkMinus(values[i]);
         }
-        return add;
+        return numbers;
+    }
+
+    private static int checkMinus(String value){
+        if(Integer.parseInt(value) < 0 ) {
+            throw new RuntimeException();
+        }
+        return Integer.parseInt(value);
+    }
+
+    private int sum(int[] numbers){
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static String[] getSplit(String str) {
+        return str.split(",|:|\n");
+    }
+
+    private static boolean isBlank(String str) {
+        return str == null || str.isEmpty();
     }
 }
